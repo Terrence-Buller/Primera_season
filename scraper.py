d06736fa4cd72f294c6ca78d4dda1905d06e0067
Terrence-Buller <130238180+Terrence-Buller@users.noreply.github.com>
2023-09-11 14:45:04 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -38,29 +38,31 @@ position_mapping = {
 "por": "Goal keeper", 'ltd': 'Right-back', 'lti': 'Left-back', 'cen': 'Centre-back', 'def': 'Defender',
 'mig': 'Midfield', 'dav': 'Striker', 'dac': 'Forward'}
 
-### Season 2000/2001 ###
+### Club_numbers ###
 site_numbers = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22",
                 "23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42",
                 "43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","78","79","80"]
+
 years = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022]
+
+### for each year run loop trough different clubs ###
 for year in years:
     for number in site_numbers:
 
-        ### url + year same 2000-2009 ###
+        ### adjust the year to the right url ###
         chosenyear = year
         chosenyear1 = int(str(chosenyear)[-2:]) + 1
         chosenyear1_formatted = f"{chosenyear1:02}"
 
         url_variable = f"{chosenyear}-{chosenyear1_formatted}"
-        # url = f"https://www.bdfutbol.com/en/t/t{url_variable}20{number}.html"
         url = f"https://www.bdfutbol.com/en/t/t{url_variable}{number}.html"
 
 
-
+        ### naming for the year in the database ###
         till_year = int(chosenyear)+1
         year_names_list = f"{chosenyear}/{till_year}"
 
-
+        ### printing the link case of errors ###
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -70,7 +72,9 @@ for year in years:
 
         print(number, url)
         soup = BeautifulSoup(response.text, "html.parser")
-        ### Club ###
+
+      
+        ### Making sure the team played primera division not segunda division ###
         club_names = soup.find_all("span", class_="heroh1")
         club_names_list = [club.find("a").get_text() for club in club_names]
         club_value = club_names_list[0]
@@ -86,7 +90,7 @@ for year in years:
             print(f"Site number {number} does not contain 'First Division' in href. Skipping.")
             continue
 
-        ### afwijking aantal managers: ###
+        ### condition if the amount of managers is different ###
         amount_manager = soup.find_all("table" , class_="taula_estil mb-3")
         total_tr_elements = 0
         for table_element in amount_manager:
@@ -97,13 +101,13 @@ for year in years:
             xyz_r = (xyz_i*2)+5
             soup_list = soup.find_all("td", class_="fit")
 
-            ### Country_names ### [1:] amount of coaches ###
+            ### Country names ###
             country_names = soup.find_all("div", class_="pais")[xyz_i:]
             countries = [country_mapping.get(country["class"][1], country["class"][1]) for country in country_names]
             # print(countries)
             # print(len(countries))
 
-            ### Player_Names ### [1:] amount of coaches
+            ### Player Names ###
             player_names = soup.find_all("span", class_="d-none d-md-block float-left")[xyz_i:]
             players = [player.get_text(strip=True) for player in player_names]
             # print(players)
@@ -115,18 +119,17 @@ for year in years:
             # print(positions)
             # print(len(positions))
 
-            ### Number ###
+            ### Number ### 
             elements_number = soup.find_all(["td", "div"], class_=["fit text-right pr-0", "vora-left-div filial"])
             element_dict = {}
 
-            # Loop through the elements and assign each one a unique identifier
+            ### loop through all numbers and give them a unique identifier ###
             for idx, element in enumerate(elements_number):
                 element_dict[element] = idx
 
-            # Initialize an empty list to store the extracted numbers and their corresponding unique identifiers
             number_elements = []
 
-            # Loop through the elements and extract numbers along with their identifiers
+            ### Loop through the elements and take both identefier and integer ###
             for element in elements_number:
                 text = element.get_text(strip=True)
                 if text.isdigit():
@@ -135,11 +138,9 @@ for year in years:
                     number = 0
                 number_elements.append((number, element_dict[element]))
 
-            # Sort the list of tuples by the unique identifiers
+            ### Sorting the list by unique identifiers, so that the order remains ###
             number_elements.sort(key=lambda x: x[1])
             numbers = [num for num, _ in number_elements]
-            print(numbers)
-            print(len(numbers))
 
             ### Age ###
             age_list = soup.find_all("td", class_="fit font-weight-bold vora-right")
@@ -147,106 +148,68 @@ for year in years:
             print(age)
             print(len(age))
 
+            ### Not all teams have called up, this takes care of both cases ###
             specific_th = soup.find('th', class_='fit', title='Called up (without playing)')
 
             if specific_th:
-                ### Played Matches
+                ### Played Matches ###
                 pm_numbers = [int(td.get_text()) for td in soup_list[xyz_r::14] if td.get_text().strip() != '']
-                # print(pm_numbers)
-                # print(len(pm_numbers))
-
+                
                 ### Started Matches ###
                 sm_numbers = [int(td.get_text()) for td in soup_list[xyz_r+1::14] if td.get_text().strip() != '']
-                # print(pm_numbers)
-                # print(len(pm_numbers))
 
                 ### Complete Matches ###
                 cm_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+2::14] if td.get_text().strip() != '']
-                # print(cm_numbers)
-                # print(len(cm_numbers))
 
                 ### Subsitute Matches ###
                 sub_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+3::14] if td.get_text().strip() != '']
-                # print(sub_numbers)
-                # print(len(sub_numbers))
 
                 ### Played minutes ###
                 min_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+5::14] if td.get_text().strip() != '']
-                # print(min_numbers)
-                # print(len(min_numbers))
 
                 ### Yellow cards ###
                 yel_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+6::14] if td.get_text().strip() != '']
-                # print(yel_numbers)
-                # print(len(yel_numbers))
 
                 ### Red cards ###
                 red_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+7::14] if td.get_text().strip() != '']
-                # print(red_numbers)
-                # print(len(red_numbers))
 
                 ### Goals ###
                 goals_numbers = [int(td.get_text()) for td in soup_list[xyz_r+8::14] if td.get_text().strip() != '']
                 goals_numbers2 = [0 if num <= 0 else num for num in goals_numbers]
-                # print(goals_numbers2)
-                # print(len(goals_numbers2))
 
                 ### Goalie Goals Against ###
                 goals_against = ['-' if (isinstance(num, int) and num >= 0) else abs(num) for num in goals_numbers]
-                print(f"Number of players: {len(players)}")
-
-
 
             else:
                 ### Played Matches
                 pm_numbers = [int(td.get_text()) for td in soup_list[xyz_r::13] if td.get_text().strip() != '']
-                # print(pm_numbers)
-                # print(len(pm_numbers))
 
                 ### Started Matches ###
                 sm_numbers = [int(td.get_text()) for td in soup_list[xyz_r+1::13] if td.get_text().strip() != '']
-                # print(sm_numbers)
-                # print(len(sm_numbers))
 
                 ### Complete Matches ###
                 cm_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+2::13] if td.get_text().strip() != '']
-                # print(cm_numbers)
-                # print(len(cm_numbers))
 
                 ### Subsitute Matches ###
                 sub_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+3::13] if td.get_text().strip() != '']
-                # print(sub_numbers)
-                # print(len(sub_numbers))
 
                 ### Played minutes ###
                 min_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+4::13] if td.get_text().strip() != '']
-                # print(min_numbers)
-                # print(len(min_numbers))
 
                 ### Yellow cards ###
                 yel_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+5::13] if td.get_text().strip() != '']
-                # print(yel_numbers)
-                # print(len(yel_numbers))
 
                 ### Red cards ###
                 red_numbers  = [int(td.get_text()) for td in soup_list[xyz_r+6::13] if td.get_text().strip() != '']
-                # print(red_numbers)
-                # print(len(red_numbers))
 
                 ### Goals ###
                 goals_numbers = [int(td.get_text()) for td in soup_list[xyz_r+7::13] if td.get_text().strip() != '']
                 goals_numbers2 = [0 if num <= 0 else num for num in goals_numbers]
-                # print(goals_numbers2)
-                # print(len(goals_numbers2))
 
                 ### Goalie Goals Against ###
                 goals_against = ['-' if (isinstance(num, int) and num >= 0) else abs(num) for num in goals_numbers]
-                # print(goals_against)
-                # print(len(goals_against))
-                print(f"Number of players: {len(players)}")
 
-
-
+            
             df = pd.DataFrame({
                 "Name": players,
                 "Country": countries,
@@ -264,19 +227,19 @@ for year in years:
                 "Goals Against": goals_against
             })
 
-            ### main squad/ substitute ###
+            ### Splitting lineup and subsitute ###
             df["Status"] = "Substitute"
             df.loc[:10, "Status"] = "Lineup"
             df['year'] = year_names_list
             df['club'] = club_value
 
-            ### change column order
+            ### change column order ###
             data = df.reindex(columns=["year", "club","Status", "#", "Name", "Country", "Age", "Played Matches", "Started Matches",
                                         "Completed Matches", "Substitute Matches", "Minutes Played", "Yellow cards", "Red Cards",
                                         "Goals", "Goals Against"])
 
 
-            ## connect ###
+            ### connect ###
             mydb = mysql.connector.connect(
                 host="***",
                 user="***",
